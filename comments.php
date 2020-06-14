b277408c6e2ccdb5cc094cbe7d152d60597bdce6
616659 <616659@users.noreply.github.com>
2020-06-14 23:01:49 +0900

    php test

--- comments.php
+++ comments.php
@@ -1,41 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>
-    Comments
-</title>
+    <head>
+        <title> Comments </title>
+    </head>
+    <body>
+        <a href="/index.html">
+           <img src="/picture/logo.png" width="200" height="200">
+       </a>
+        <h1> Please leave a comments </h1>
 
-</head>
-<body>
-    <a href="/index.html">
-        <img src="/picture/logo.png" width="200" height="200">
-    </a>
-<h1>
-    Pleas leave a comments
-</h1>
+       <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+           Name <input type="text" name="name"><br/>
+           Email <input type="email" name="email"><br/>
+           Comment <input type="text"  name="comment"><br/>
+         <input type="submit" value="Post Comment">
+        </form>
 
-<?php include('comments.php') ?>
+       <h1> Comments </h1>
 
-    <form action="#" method="post">
-        Name <input type="text" name="name"><br/>
-        Email <input type="email" name="email"><br/>
-        Comment <input type="text"  name="comment"><br/>
-        <input type="submit" value="Post Comment">
-    </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_REQUEST['name'];
+            $email = $_REQUEST['email'];
+            $text = $_REQUEST['text'];
 
-    <?php
-        $myfile = fopen("pcomments.html","a") or die("there is an error");
-        $writeinfile = "<br> Name:</br>".$_POST['name']."<br/>";
-        $writeinfile = "<br> Email:</br>".$_POST['email']."<br/>";
-        $writeinfile = "<br> Comment:</br>".$_POST['comment']."<br/>I<hr>";
-        fwrite($myfile, $writeinfile);
-        fwrite($myfile, $writeinfile2);
-        fwrite($myfile, $writeinfile3);
-        fclose($myfile);
-        
-        require ("comments.html");
+                echo $name;
+                echo $email;
+                echo $text;
+            }
+        ?>
 
-    ?>
-
-</body>
+    </body>
 </html>
