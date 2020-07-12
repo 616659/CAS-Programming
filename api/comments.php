0f7f3c816d15cd8e6c0a546f07d90fe779e4c87c
616659 <616659@users.noreply.github.com>
2020-07-12 11:38:43 +0900

    Trying to make comment section work by using SQL

--- api/comments.php
+++ api/comments.php
@@ -19,16 +19,47 @@
        <h1> Comments </h1>
 
         <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-               $name = $_REQUEST['name'];
-               $email = $_REQUEST['email'];
-               $comment = $_REQUEST['comment'];
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $name = $_REQUEST['name'];
+                $email = $_REQUEST['email'];
+                $comment = $_REQUEST['comment'];
                
-               echo $name;
-               echo "\n";
-               echo $email;
-               echo "\n";
-               echo $comment;
+                $serverName = "125.182.209.94";
+                $username = "Vercel";
+                $password = "CASProj2020!";
+                $dBName = "ESiSBDB";
+
+                $conn = new mysqli ($serverName, $username, $password, $dBName);
+
+                //detect any connection error
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "INSERT INTO commentSection (name, email, comment)
+                VALUES ($name, $email, $comment)";
+
+                //testing if SQL works or not, will be removed once it is confirmed to work
+                if ($conn->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                } else {
+                     echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+
+                //select data from table
+                $sql = "SELECT name, email, comment FROM commentSection";
+                
+                $result = $conn->query($sql);
+
+                //output data selected
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                      echo $row["name"]. " (" . $row["email"]. ")<br>" . $row["comment"] . "<br><br>";
+                    }
+                } else {
+                    echo "No results";
+                }
+                $conn->close();
             }
         ?>
 
